7b896874ea78a04b4f6d2373073bda7b9697c9e9
FREEWING-JP <freewing.jp@gmail.com>
2024-03-29 14:15:09 +0900

    fix issue 16

--- 15_Copy_Fritzing_Requirement_DLL_files.bat
+++ 15_Copy_Fritzing_Requirement_DLL_files.bat
@@ -11,6 +11,9 @@ copy .\zlib-src\build64\Release\zlib.dll .\release64
 copy .\quazip-6.5.3-1.4\bin\quazip1-qt6.dll .\release64
 copy C:\Qt\6.5.3\msvc2019_64\bin\Qt6Core5Compat.dll .\release64
 
+rem for Simulate function
+copy .\Spice64_dll\dll-vs\*.dll .\release64
+
 rem for Fritzing 1.0.2
 copy .\clipper1\6.4.2\lib\polyclipping.dll .\release64
 
