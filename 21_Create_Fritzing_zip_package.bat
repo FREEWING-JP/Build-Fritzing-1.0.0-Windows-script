fcf548ac4ea65cfc992b698640a13555b552f5d6
FREEWING-JP <freewing.jp@gmail.com>
2024-10-28 23:46:21 +0900

    fix Cannot change interface language #24

--- 21_Create_Fritzing_zip_package.bat
+++ 21_Create_Fritzing_zip_package.bat
@@ -1,5 +1,5 @@
 @echo off
-rem 20_Create_Fritzing_zip_package.bat
+rem 21_Create_Fritzing_zip_package.bat
 rem http://www.neko.ne.jp/~freewing/software/build_fritzing_100_windows/
 rem Copyright (c) 2023-2024 FREE WING,Y.Sakamoto
 echo %0
